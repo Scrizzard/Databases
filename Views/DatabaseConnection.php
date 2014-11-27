ecc870b49d989f77c0ffb70229022b5ce2f04896
Alex Keeling <alex@keeling.me>
2014-11-27 16:17:17 -0400

    updates php DB connection with usage example

--- Views/DatabaseConnection.php
+++ Views/DatabaseConnection.php
@@ -3,6 +3,10 @@
 	// rather than typing out the connection information in dozens of different
 	// files
 
+	// to use in your php files:
+	// include '../DatabaseConnection.php';
+	
+
 	// the database connection
 	// you may need to change this login information for your specific installation
 	$SERVER = 'localhost';
