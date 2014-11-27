f92226aa083266a654e079267fc8b12bc7ab2829
carolinefquinn <carolinefquinn@gmail.com>
2014-11-27 10:33:06 -0400

    Update employee.php

--- Views/employee/employee.php
+++ Views/employee/employee.php
@@ -1,19 +1,25 @@
 <?php
 mysql_connect("localhost", "root", "");
 mysql_select_db("tms");
+
+
 $query =
 "SELECT stationOfOrigin, terminalStation, departureDate, departureTime, arrivalDate, arrivalTime FROM EmployeeVoyagePair
 INNER JOIN Voyage
 ON employeeVoyagePair.voyageID = Voyage.id
 INNER JOIN TrainRoute
 ON Voyage.routeID = TrainRoute.routeID
-WHERE employeeID = \"" . $_GET["employeeID"] . "\"";
+WHERE employeeID =$_POST['employeeID'];
+
 $result = mysql_query($query);
-if (!$result) {
+if (!$result) 
+{
 die('Invalid query: ' . mysql_error());
 }
 $rows = array();
-while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+
+while($row = mysql_fetch_array($result, MYSQL_ASSOC)) 
+{
 $rows[] = $row;
 }
 echo json_encode($rows);
