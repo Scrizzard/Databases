5632f5f64f7dbd0a5baef7dadc315012a6184e72
Alex Keeling <alex@keeling.me>
2014-11-27 16:15:24 -0400

    Adds a php Database Connection file

---
+++ Views/DatabaseConnection.php
@@ -0,0 +1,16 @@
+<?php
+	// Include this file in any php file that needs to access the database, 
+	// rather than typing out the connection information in dozens of different
+	// files
+
+	// the database connection
+	// you may need to change this login information for your specific installation
+	$SERVER = 'localhost';
+	$USER = 'root';
+	$PASSWORD = 'openSesame';
+	$DATABASENAME = 'TMS';
+
+	$DBC = mysql_connect($SERVER, $USER, $PASSWORD);
+	mysql_select_db($DATABASENAME);
+
+?>
